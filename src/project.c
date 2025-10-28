f8a8944f15ff68fda9d18977e38b30d2c13dec48
AatiSamuel <aati.strommer@gmal.com>
2025-10-28 23:12:47 +0200

    project.c added to the root src folder

---
+++ src/project.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <pico/stdlib.h>
+#include <FreeRTOS.h>
+#include <queue.h>
+#include <task.h>
+#include "tkjhat/sdk.h"
+// Copied and pasted these from main.c
+
+// This file to be used for the project itself. Main.c was exercise session 2.
+// Although the tasks are missing from it because it was never pushed to the repository.
+
+
