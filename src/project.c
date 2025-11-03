30ea24fd30bd4adc3e8570a596b1d85d5ad0a7db
AatiSamuel <aati.strommer@gmal.com>
2025-11-03 22:43:30 +0200

    displayOutput added

--- src/project.c
+++ src/project.c
@@ -10,4 +10,28 @@
 // This file to be used for the project itself. Main.c was exercise session 2.
 // Although the tasks are missing from it because it was never pushed to the repository.
 
+typedef enum {
+    WAITING,
+    DISPLAYING,
+    DATA_READY
+} programState;
 
+char current_character;
+char current_str[]; // string of all the characters received so far
+programState state = WAITING;
+
+// I'm guessing we should make a statemachine task that uses switch statements
+// to determine which state we're in?
+
+
+
+
+// Terminal communication
+void displayOutput(char current_str[]) {
+    /*
+    Displays the current morse code string and resets it.
+    */
+    printf("%s\n", current_str);
+    current_str[0] = "\0"; // Reset string after displaying
+    state = WAITING;
+}
